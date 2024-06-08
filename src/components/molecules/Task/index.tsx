ddf4e1abdda8408aaeb67a0ccf0123ccbb8164b8
Rafael Morais <rafaelcanical@gmail.com>
2024-06-08 11:22:37 +0200

    Play chime when completing a task and complete it in real time

--- src/components/molecules/Task/index.tsx
+++ src/components/molecules/Task/index.tsx
@@ -1,16 +1,34 @@
 import { TTask, useToggleTaskStatus } from '@/queries/tasks'
 import styles from './styles.module.css'
 import Checkbox from '@/components/atoms/Checkbox'
+import { useEffect, useState } from 'react'
+import { competeTaskChime } from '@/utils/audio'
 
 type TTaskProps = {
   task: TTask
 }
 
 const Task = ({ task }: TTaskProps) => {
+  // Local state
+  const [localTask, setTask] = useState<TTask>(task)
+
+  // When task changes from parent
+  useEffect(() => {
+    if (task != null) setTask(task)
+  }, [task])
+
   /**
    * Toggle as completed/uncompleted
    */
   const toggle = () => {
+    const t = { ...localTask }
+    t.isCompleted = !t.isCompleted
+    setTask(t)
+
+    if (t.isCompleted) {
+      competeTaskChime()
+    }
+
     toggleTaskStatus()
   }
 
@@ -20,8 +38,8 @@ const Task = ({ task }: TTaskProps) => {
   return (
     <div className={styles.row}>
       <div className={styles.text}>
-        <Checkbox checked={task.isCompleted} toggle={toggle} />
-        <span>{task.text}</span>
+        <Checkbox checked={localTask.isCompleted} toggle={toggle} />
+        <span>{localTask.text}</span>
       </div>
       <div className={styles.status}>-</div>
     </div>
